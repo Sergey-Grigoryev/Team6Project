cd1ffee1fc864511b3917dde6a8d1a29ec5020b9
ruserge88 <ruserge88@gmail.com>
2021-06-12 15:06:35 -0700

    JS psudo code

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+var saveItemBtn = document.querySelector("");
+var saveItemName = document.querySelector("");
+var saveItemDuration = document.querySelector("");
+
+
+// on load - get localStorage info:
+
+
+// Save Purchased Item to tracking list:
+var saveToTrack = function (itemName) {
+
+};
+
+
+
+// API call to nutrition facts:
+
+
+
+// API call to Wine Paring:
+
+
+
+// click event listener
+
+
+
+// Drag & Drop Dom manipulation code:
+
+
+
